8c435b251a12336ab7916ebad953c3fa3aa04149
pooneetthaper <pooneetthaper@gmail.com>
2015-02-23 09:27:23 -0500

    ummmm

---
+++ 03-Problems/CowStomps.java
@@ -0,0 +1,22 @@
+pubic class CowStomps{
+
+    private int[][] pasture= {{28,25,20,32,34,36},
+			      {27,25,20,20,30,34},
+			      {24,20,20,20,20,30},
+			      {20,20,14,14,20,20}};
+    private int pastureX;
+    private int pastureY;
+
+    public CowStomps(){
+	CowStomps(6,4);
+    }
+
+    public CowStomps(int x,int y){
+	pastureX=x;
+	pastureY=y;
+    }
+
+    public void stomp(int R_s, int C_s, int D_s){
+	
+    }
+}

8daacd86ae15c57dd6bab93fc127f384e80f3b35
pooneetthaper <pooneetthaper@gmail.com>
2015-02-26 09:28:11 -0500

    working on it

--- 04-merge/Merging.java
+++ 04-merge/Merging.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Merging{
     
     public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
@@ -9,6 +11,11 @@ public class Merging{
 	    else {
 		c.add(b.remove(0));
 	    }
+	    //bug fixing
+	    System.out.println(a);
+	    System.out.println(b);
+	    System.out.println("");
+	    //end debugging
 	}
 	if (a.size()>0){
 	    for (int i=0;i<a.size();i++){
@@ -23,15 +30,37 @@ public class Merging{
 	return c;
     }
 
+    public ArrayList<Integer> msort(ArrayList<Integer> L){
+	if (L.size()<2) return L;
+	else {
+	    ArrayList<Integer> a= new ArrayList<Integer>();
+	    for(int i=0; i<L.size()/2;i++){
+		a.add(L.get(i));
+	    }
+	    ArrayList<Integer> b= new ArrayList<Integer>();
+	    for(int i=L.size()/2; i<L.size();i++){
+		b.add(L.get(i));
+	    }
+	    return merge(msort(a),msort(b));
+	}
+    }
+
     public static void main(String[] args){
 	Merging m = new Merging();
 	ArrayList<Integer> b=new ArrayList<Integer>();
 	ArrayList<Integer> a=new ArrayList<Integer>();
 	for (int i=0; i<10; i++){
-	    a.add(Math.ceil(Math.random()*100));
-	    b.add(Math.ceil(Math.random()*100));
+	    a.add((int)Math.ceil(Math.random()*100));
+	    b.add((int)Math.ceil(Math.random()*100));
 	}
-	m.merge(a,b);
+	System.out.println(a.size());
+	System.out.println(b.size());
+	ArrayList<Integer> c = m.merge(msort(a),msort(b));
+	System.out.println(c.size());
+	System.out.println(c);
+	ArrayList<Integer> d=m.msort(c);
+	System.out.println(d);
+	System.out.println(d.size());
     }
 
 }

33cc81c7c02533f8ce024a0de9e87b795b80772e
pooneetthaper <pooneetthaper@gmail.com>
2015-02-27 09:16:58 -0500

    merging functional

--- 04-merge/Merging.java
+++ 04-merge/Merging.java
@@ -7,41 +7,43 @@ public class Merging{
 	while (a.size()>0 && b.size()>0){
 	    if (a.get(0) < b.get(0)){
 		c.add(a.remove(0));
+		System.out.println(c);
 	    }
 	    else {
 		c.add(b.remove(0));
-	    }
-	    //bug fixing
-	    System.out.println(a);
-	    System.out.println(b);
-	    System.out.println("");
-	    //end debugging
-	}
-	if (a.size()>0){
-	    for (int i=0;i<a.size();i++){
-		c.add(a.remove(i));
+		System.out.println(c);
 	    }
 	}
-	else if (b.size()>0){
-	    for (int i=0;i<b.size();i++){
-		c.add(b.remove(i));
-	    }
+	System.out.println("");
+	
+	while (a.size()>0){
+		c.add(a.remove(0));
+		System.out.println(c);
+	}
+        while (b.size()>0){
+		c.add(b.remove(0));
+		System.out.println(c);
 	}
 	return c;
     }
 
     public ArrayList<Integer> msort(ArrayList<Integer> L){
-	if (L.size()<2) return L;
+	if (L.size()<=1) return L;
 	else {
+	    int d=L.size()/2;
 	    ArrayList<Integer> a= new ArrayList<Integer>();
-	    for(int i=0; i<L.size()/2;i++){
+	    for(int i=0; i<d;i++){
 		a.add(L.get(i));
 	    }
+
 	    ArrayList<Integer> b= new ArrayList<Integer>();
-	    for(int i=L.size()/2; i<L.size();i++){
+	    for(int i=d; i<L.size();i++){
 		b.add(L.get(i));
 	    }
-	    return merge(msort(a),msort(b));
+
+	    ArrayList<Integer> c= merge(msort(a),msort(b));
+	    System.out.println(c);
+	    return c;
 	}
     }
 
@@ -53,14 +55,9 @@ public class Merging{
 	    a.add((int)Math.ceil(Math.random()*100));
 	    b.add((int)Math.ceil(Math.random()*100));
 	}
-	System.out.println(a.size());
-	System.out.println(b.size());
-	ArrayList<Integer> c = m.merge(msort(a),msort(b));
-	System.out.println(c.size());
+
+	ArrayList<Integer> c = m.merge(m.msort(a),m.msort(b));
 	System.out.println(c);
-	ArrayList<Integer> d=m.msort(c);
-	System.out.println(d);
-	System.out.println(d.size());
     }
 
 }

97108519eb3b80a1645ba21678fcdb485e96006e
pooneetthaper <pooneetthaper@gmail.com>
2015-03-05 23:26:12 -0500

    completed)

--- 03-Problems/CowStomps.java
+++ 03-Problems/CowStomps.java
@@ -1,4 +1,4 @@
-pubic class CowStomps{
+public class CowStomps{
 
     private int[][] pasture= {{28,25,20,32,34,36},
 			      {27,25,20,20,30,34},
@@ -7,16 +7,73 @@ pubic class CowStomps{
     private int pastureX;
     private int pastureY;
 
+    /*
     public CowStomps(){
 	CowStomps(6,4);
     }
+    */
 
     public CowStomps(int x,int y){
 	pastureX=x;
 	pastureY=y;
     }
 
-    public void stomp(int R_s, int C_s, int D_s){
+    public void stomp(int R, int C, int stomps){
+	int max=0;
+	for (int i=R-1;i<R+2;i++){
+	    for (int j=C-1;j<C+2;j++){
+		if (pasture[i][j]>max) max=pasture[i][j];
+	    }
+	}
+
+	max-= stomps;
 	
+	for (int i=R-1;i<R+2;i++){
+	    for (int j=C-1;j<C+2;j++){
+		if (pasture[i][j]>max){
+		    pasture[i][j]=max;
+		}
+	    }
+	}
+    }
+
+    public int TDepth(int Elev){
+	int TDepth=0;
+	for (int r=0;r<pasture.length;r++){
+	    for (int c=0;c<pasture[0].length;c++){
+		if (pasture[r][c]>Elev) pasture[r][c]=0;
+		else{
+		    int a= Elev-pasture[r][c];
+		    pasture[r][c]=a;
+		    TDepth += a;
+		}
+	    }
+	}
+	return TDepth;
+    }
+
+    public int volume(int Elev){
+	return TDepth(Elev)*72*72;
+    }
+
+    public String toString(){
+	String s="";
+	for (int r=0;r<pasture.length;r++){
+	    for (int c=0;c<pasture[0].length;c++){
+		s=s+pasture[r][c]+" ";
+	    }
+	    s=s+"\n";
+	}
+	return s;
+    }
+
+    public static void main(String[] args){
+	CowStomps c= new CowStomps(6,4);
+	System.out.println(c);
+	c.stomp(1,4,4);
+	System.out.println(c);
+	c.stomp(1,1,10);
+	System.out.println(c);
+	System.out.println(c.volume(22));
     }
 }

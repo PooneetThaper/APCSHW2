058e90522abbdb83bc53ab8ae54f31040e1622db
pooneetthaper <pooneetthaper@gmail.com>
2015-03-16 09:26:58 -0400

    did find, almost done with insert

--- 04-merge/Merging.java
+++ 04-merge/Merging.java
@@ -45,13 +45,13 @@ public class Merging{
 	Merging m = new Merging();
 	ArrayList<Integer> b=new ArrayList<Integer>();
 	ArrayList<Integer> a=new ArrayList<Integer>();
-	for (int i=0; i<5; i++){
+	for (int i=0; i<200000; i++){
 	    a.add((int)Math.ceil(Math.random()*100));
 	    b.add((int)Math.ceil(Math.random()*100));
 	}
 
 	ArrayList<Integer> c = m.merge(m.msort(a),m.msort(b));
-	System.out.println(c);
+	//System.out.println(c);
     }
 
 }

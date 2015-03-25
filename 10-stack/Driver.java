4c7a5c5e174194d213df5ddfaaa71137d72aa6d1
pooneetthaper <pooneetthaper@gmail.com>
2015-03-25 09:12:35 -0400

    progress

--- 10-stack/Driver.java
+++ 10-stack/Driver.java
@@ -2,20 +2,20 @@ public class Driver{
     public static void main(String[] args){
 	myStack<Integer> m=new myStack<Integer>();
 
-	System.out.println("Empty: "+empty());
+	System.out.println("Empty: "+m.empty());
        
 	for(int i=8;i>=0;i--){
 	    m.push(i);
 	}
 
-	System.out.println(top);
+	System.out.println(m.top());
 
-	System.out.println("Empty: "+empty());
+	System.out.println("Empty: "+m.empty());
 	
-	for(int n:myStack){
+	while(!m.empty()){
 	    System.out.print(m.pop()+", ");
 	}
 
-	System.out.println("Empty: "+empty());
+	System.out.println("Empty: "+m.empty());
     }
 }

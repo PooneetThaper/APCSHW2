75478fd2a04c5c890252d93283205daaf700d66f
pooneetthaper <pooneetthaper@gmail.com>
2015-04-27 09:27:28 -0400

    working toString

--- 14-BinarySearchTree/BST.java
+++ 14-BinarySearchTree/BST.java
@@ -2,12 +2,16 @@ public class BST{
     private Node r;
     
     public BST(){
-	r=new Node(5);
     }
 
     public void insert(Node t, int i){
+	Node n= new Node(i);
 	Node temp1=t;
 	Node temp2=t;
+	if (r==null){
+	    r=n;
+	    return;
+	}
 	//assumes that root is not null
         while(temp2!=null){
 	    temp1=temp2;
@@ -19,11 +23,11 @@ public class BST{
 	}
 	if (temp2==null){
 	    if (i<temp1.getdata()){
-		temp2=new Node(i);
+		temp2=n;
 		temp1.setleftChild(temp2);
 	    }
-	    if(i>temp1.getdata()){
-		temp2=new Node(i);
+	    else if(i>temp1.getdata()){
+		temp2=n;
 		temp1.setrightChild(temp2);
 	    }
 	}
@@ -45,10 +49,34 @@ public class BST{
 	return null;
     }
 
+    public String down(Node n){
+	if (n==null) return "";
+	String s=n.getdata()+", ";
+	s=s+down(n.getleftChild());
+	s=s+down(n.getrightChild());
+	return s;
+    }
+
+    public String ascending(Node n){
+	if (n==null) return "";
+	String s="";
+	s=s+ascending(n.getleftChild());
+	s=s+n.getdata()+", ";
+	s=s+ascending(n.getrightChild());
+	return s;
+    }
+
+    public String toString(){
+	return ascending(r);
+    }
+
     public static void main(String[] args){
 	BST a=new BST();
+	a.insert(a.r,5);
 	a.insert(a.r,4);
 	a.insert(a.r,6);
 	System.out.println(a.search(a.r,4));
+
+	System.out.println(a.toString());
     }
 }

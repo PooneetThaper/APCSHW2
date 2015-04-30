102e52fc8105a19e27e8d27ff3e1ff86dc33e6e7
pooneetthaper <pooneetthaper@gmail.com>
2015-04-30 09:27:52 -0400

    trys made

--- 14-BinarySearchTree/BST.java
+++ 14-BinarySearchTree/BST.java
@@ -70,27 +70,48 @@ public class BST{
     }
 
     public void remove(Node t, int i){
-        Node temp1=t;
-	Node temp2=t;
+        Node temp1=new Node(Integer.MIN_VALUE);
+	Node temp2=temp1;
+	temp1.setrightChild(t);
+	Boolean right=false;
 	while(temp2!=null){
 	    temp1=temp2;
 	    if(i<temp1.getdata()){
 		temp2=temp1.getleftChild();
-		if(temp2.getdata()==i) 
+		if(temp2.getdata()==i){
+		    right=false;
 		    break;
+		}
 	    }else if(i>temp1.getdata()){
 		temp2=temp1.getrightChild();
-		if(temp2.getdata()==i) 
+		if(temp2.getdata()==i){
+		    right=true;
 		    break;
+		}
 	    }
 	}
 	if(temp2!=null&&temp2.getdata()==i){
-	    if (temp2.getleftChild()==null||
-		temp2.getrightChild()==null){
-		if(temp1.getdata()>temp2.getdata()){
-		    
+	    if (temp2.getleftChild()==null){
+		if(right){
+		    temp1.setrightChild(temp2.getrightChild());
+		}else{
+		    temp1.setleftChild(temp2.getrightChild());
 		}
-		    
+	    } if (temp2.getrightChild()==null){
+		if(right){
+		    temp1.setrightChild(temp2.getleftChild());
+		}else{
+		    temp1.setleftChild(temp2.getleftChild());
+		}
+	    }else{
+		Node temp3=temp2.getleftChild();
+		while(temp3.getrightChild()!=null){
+		    temp3=temp3.getrightChild();
+		}
+		temp2.setdata(temp3.getdata());
+		remove(temp2,temp3.getdata());
+	    }
+	}
     }
 
     public static void main(String[] args){
@@ -99,10 +120,12 @@ public class BST{
 	a.insert(a.r,4);
 	a.insert(a.r,6);*/
 	for (int i=0;i<10;i++){
-	    a.insert(a.r,(int)Math.ceil(Math.random()*10));
+	    a.insert(a.r,i);
 	}
 	System.out.println(a.search(a.r,4));
 
+	System.out.println(a.toString());
+	a.remove(a.r,9);
 	System.out.println(a.toString());
     }
 }
